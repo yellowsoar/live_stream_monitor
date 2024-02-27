0b865a73b3b6a4e41283ae7e3550e6d1e8980c34
yellowsoar <yellowsoar@gmail.com>
2024-02-27 16:51:48 +0800

    feat: video submit page

--- src/youtube_live/views.py
+++ src/youtube_live/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from youtube_live.forms import *
+from youtube_live.models import *
+
+
+def submit(request):
+    if request.method == "GET":
+        context = {}
+        context['form'] = SubmitMonitoredVideoForm()
+        return render(
+            request,
+            "submit.html",
+            context,
+        )
