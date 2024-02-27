ae755b6a6806896c51991fd837f38c605716eee5
yellowsoar <yellowsoar@gmail.com>
2024-02-27 17:04:22 +0800

    feat: monitor YTB video until status not is_live

--- src/youtube_live/apps.py
+++ src/youtube_live/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class YoutubeLiveConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "youtube_live"
+
+    def ready(self):
+        import youtube_live.signals
---
+++ src/youtube_live/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from youtube_live.models import *
+from youtube_live.tasks import *
+from youtube_live.utils import *
+
+
+@receiver(
+    post_save,
+    sender=MonitoredVideo,
+)
+def send_to_task_queue(
+    sender,
+    instance,
+    created,
+    **kwargs,
+):
+    check_video.apply_async(
+        (instance.video_id,),
+        retry=True,
+        max_retries=None,
+        countdown=instance.check_duration,
+    )
