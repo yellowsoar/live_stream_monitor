5e78b92ca02cb8a038a2a74d2f5f4f57e76c609f
yellowsoar <yellowsoar@gmail.com>
2024-02-27 17:00:46 +0800

    feat: monitered YTB video to Django admin

--- src/youtube_live/admin.py
+++ src/youtube_live/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from youtube_live.models import *
+
+
+class MonitoredVideoAdmin(admin.ModelAdmin):
+    class Meta:
+        model = MonitoredVideo
+
+
+admin.site.register(
+    MonitoredVideo,
+    MonitoredVideoAdmin,
+)
