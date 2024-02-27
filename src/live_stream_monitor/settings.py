2129a5295bde47dce057af262e713ef869b9a1d3
yellowsoar <yellowsoar@gmail.com>
2024-02-28 00:30:05 +0800

    fix: missing ALLOWED_HOSTS issue

--- src/live_stream_monitor/settings.py
+++ src/live_stream_monitor/settings.py
@@ -36,7 +36,7 @@ lsm_utils.raise_exception_if_not_truth(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
