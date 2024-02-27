cf4f2601f0c49e4b1fa9f6be7bfb9323a18d3684
yellowsoar <yellowsoar@gmail.com>
2024-02-27 16:53:30 +0800

    feat: video submission page accept submit

--- src/youtube_live/views.py
+++ src/youtube_live/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+import copy
+
+from django.shortcuts import (
+    redirect,
+    render,
+)
 
 from youtube_live.forms import *
 from youtube_live.models import *
@@ -13,3 +18,25 @@ def submit(request):
             "submit.html",
             context,
         )
+
+    if request.method == "POST":
+        # Get video info
+        request_data = request.POST.dict()
+        video_info = get_video_info(request_data['video_id'])
+
+        # Handle video id
+        working_data = copy.deepcopy(request_data)
+        working_data['video_id'] = ''
+        if 'id' in video_info:
+            working_data['video_id'] = video_info['id']
+
+        # Handle Form
+        form = SubmitMonitoredVideoForm(
+            working_data,
+        )
+        if not form.is_valid():
+            return redirect("submit")
+
+        # Handle validated form data
+        form.save()
+        return redirect("list")
