659f090da20765d9bf5f6657a3819c4ac5d0e2d3
yellowsoar <yellowsoar@gmail.com>
2024-02-27 16:09:35 +0800

    chore: add Django static files setting

--- src/live_stream_monitor/settings.py
+++ src/live_stream_monitor/settings.py
@@ -153,3 +153,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(
+    BASE_DIR,
+    'static',
+)
