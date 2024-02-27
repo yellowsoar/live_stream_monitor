5692b5c833b137cebb2722e626015b75bd11090d
yellowsoar <yellowsoar@gmail.com>
2024-02-27 16:29:29 +0800

    add: Youtube Video Id retrieving utility

--- src/youtube_live/utils.py
+++ src/youtube_live/utils.py
@@ -33,3 +33,21 @@ def get_video_info(
         except:
             return_data = {}
     return return_data
+
+
+def get_youtube_video_id(
+    input_string: str,
+) -> str:
+    """Return Youtube Video id from given string
+
+    Parameters
+    ----------
+    input_string : str
+        A Youtube video id or a URL
+    """
+    return_data = ''
+    video_info = get_video_info(input_string)
+    if 'id' in video_info:
+        return_data = video_info['id']
+
+    return return_data
